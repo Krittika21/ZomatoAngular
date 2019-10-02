db55137c4571da828ab24d9b68502665bdf36553
Krittika <Krittika@DESKTOP-TLP8QUF>
2019-10-02 17:51:10 +0530

    Angular

---
+++ src/app/restaurants/restaurants.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AllRestaurantsComponent } from './all-restaurants/all-restaurants.component';
+
+
+
+@NgModule({
+  declarations: [
+    AllRestaurantsComponent,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RestaurantsModule { }
