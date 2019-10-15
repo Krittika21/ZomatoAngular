95e3bf2e056632fd2437b94c71f267435a007af7
Krittika <Krittika@DESKTOP-TLP8QUF>
2019-10-15 17:40:22 +0530

    angular component

--- src/app/restaurants/restaurants.module.ts
+++ src/app/restaurants/restaurants.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllRestaurantsComponent } from './all-restaurants/all-restaurants.component';
 import { FormsModule } from '@angular/forms';
+import { DetailsComponent } from './details/details.component';
+import { MenuComponent } from './menu/menu.component';
+import { CartComponent } from './cart/cart.component';
 
 
 
 @NgModule({
   declarations: [
     AllRestaurantsComponent,
+    DetailsComponent,
+    MenuComponent,
+    CartComponent,
   ],
   imports: [
     CommonModule,
